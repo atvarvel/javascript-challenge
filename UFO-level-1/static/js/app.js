9da2ced0e9cbc2de3309a34923259d22f252321f
Alivia Varvel <atvarvel@gmail.com>
2021-06-28 23:12:20 -0400

    updated app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,29 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("#target-table");
+var filterButton = d3.select("#filter-btn");
+
+tableData.forEach(obj => {
+    var row = tbody.append("tr");
+    Object.entries(obj).forEach(([key, value]) => {
+        var data = row.append("td");
+        data.text(value);
+    });
+});
+
+filterButton.on("click", function() {
+    tbody.html("");
+    d3.event.preventDefault();
+    var input = d3.select("#datetime");
+    var inputValue = input.property("value");
+
+    var inputType = tableData.filter(one => one.datetime === inputValue);
+    inputType.forEach((selection) => {
+        var row = tbody.append("tr");
+        Object.entries(selection).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});
